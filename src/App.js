5a7a054e96d745fd309c701d44ee18ea33b4656a
mubeena <mubeena.ali@gmail.com>
2021-05-25 14:25:40 -0500

    adding wireframes and changed UI

--- src/App.js
+++ src/App.js
@@ -6,29 +6,25 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 /* HOME PAGE */
-
 function Home(){
   return(
   <>
   <div className="container">
     <div className="homeContainer">
          <h2>Welcome!</h2>
-         <h2>Click on Todo to create tasks</h2>
+       <h3>Click on Todo to create your tasks for today</h3>
     </div>
   </div> 
   </>
   ); }
 
-
   /* TODO PAGE */
-
 function Todo(){
-const [query, setQuery] = useState(null);
-const [itemList, updateList] = useState([]);
 
-//const [currentFilter, setCurrentFilter] = useState('');
-// Adds the search to list when add button clicked
-//setSearches(searches => [...searches, searches.concat(query)])
+const [query, setQuery] = useState("");
+const [itemList, updateList] = useState([]);
+const [filter, setFilter] = useState('All');
+
 
 //Adds task when add button is clicked
 const addToList = () => {
@@ -43,67 +39,89 @@ const keyPressed = ({ key }) => {
   }
 }
 
- // e.preventDefault();
-//setSearches(searches => searches.concat(query))
-//setCurrentFilter(currentFilter => currentFilter.concat(query));
-//console.log("itemlist", itemList);
-
 // Updates input box
 const updateQuery =  e => {
  setQuery(e.currentTarget.value)
 };
 
-
-  return(
+//Input Box, add button and  list of task
+return(
   <>
  <div className="container ">
   <div className=" container wrapper">
-    <div className="container d-flex justify-content-between input-wrapper">
-    <input value={query} onChange={updateQuery} onKeyPress={keyPressed} className="form-control w-75" type="text"></input>
-    <button onClick={addToList} className="btn btn-success w-25">Add</button>
-    </div>
-
-  </div>
-    <List itemslist = {itemList} updateList={updateList}/>
- </div>
   
-  </>
-  ); }
+    <div className="container d-flex justify-content-between input-wrapper">
+
+    <input value={query} onChange={updateQuery} onKeyPress={keyPressed} className="form-control w-75" type="text"></input>
+    <button onClick={addToList} className="btn btn-secondary w-25">Add</button>
+    </div>
+  </div>
+    <View itemslist = {itemList} updateList={updateList}  filter={filter} setFilter={setFilter} />
+    <List itemslist = {itemList} updateList={updateList}/>
+    
+ </div>
+</>
+); }
+
+
+const allTasks = (e) => {
+ e.itemslist.map((k) =>{
+  console.log(k, "ALL button is clicked");
+ })}
+
+
+function View(props){
+ console.log(props)
+ // View of all, incomplete and completed tasks
+  return(
+    <>
+      <div className="container ">
+        <div className=" container filter-btn ">
+         <button onClick={ ()=>allTasks(props)}  className="btn btn-secondary " >
+          All 
+          </button>
+        <button  className="btn btn-secondary " >
+          Completed 
+        </button>
+        <button className="btn btn-secondary " >
+          Incomplete
+        </button>
+        </div>
+    </div>  
+</>
+);}
 
 
 // Creates list of tasks
 function List(props){
-
-const deleteItem = (key) => {
-const newlist = props.itemslist.filter((item) => {
- return item.key !== key;
-});
-props.updateList(newlist);
-};
-
-const completeItem = (key) => {
-const itemComplete = props.itemslist.map((k) =>{
-  //console.log( k.key === key);
-if(k.key === key){
-  return{  ...k , completed: true}
- 
-}
-return key;
-})
-props.updateList(itemComplete)
-}
-
-  
+  const deleteItem = (key) => {
+  const newlist = props.itemslist.filter((item) => {
+   return item.key !== key;
+  });
+  props.updateList(newlist);
+  };
+  const completeItem = (key) => {
+  const itemComplete = props.itemslist.map((k) =>{
+    //console.log( k.key === key);
+  if(k.key === key){
+   return { ...k , completed: true}
+  }
+  return k;
+  })
+  props.updateList(itemComplete)
+  }
 
 
-console.log({props});
+// Task box with complete and delete button
 return (
 <>
 {props.itemslist.map((key) =>{
-  return <div className="container taskInputBox w-75">
-            <p className="taskBox " key={key.key}> {key.item} </p>
-            <button onClick={ ()=>completeItem(key.key) }className=" complete-Btn btn btn-sm btn-success">Complete</button>
-            <button onClick={ ()=>deleteItem(key.key)} className=" del-Btn btn btn-sm btn-success">Delete</button>
+  return <div className="container ">
+         <div className="container taskInputBox w-75  ">
+            <p className={`taskBox key={key.key} ${key.completed ? "completed ": ''}`}> {key.item} </p>
+            <button onClick={ ()=>completeItem(key.key) }className=" complete-Btn btn btn-sm btn-success ">Complete</button>
+            <button onClick={ ()=>deleteItem(key.key)} className=" del-Btn btn btn-sm btn-light">Delete</button>      
+         </div>
          </div>
 })
 }
@@ -112,39 +130,47 @@ return (
 
 
 
+
 /* CONTACT PAGE */
   function Contact(){
     return(
   <>
-  <div className ="m-2">
+  <div className="container contactContainer ">
+  <div className ="m-2 ">
   <label for ="exampleFormControlInput1" className ="form-label"> First Name:</label>
   <input type ="text" className ="form-control  " id ="FirstName" placeholde r="First Name"></input>
   </div>
   <div className="m-2 ">
   <label for="exampleFormControlInput1" className="form-label">Last Name:</label>
-  <input type="text" className="form-control" id="LastName" placeholder="Last Name:"></input>
+  <input type="text" className="form-control " id="LastName" placeholder="Last Name:"></input>
   </div>
   <div className="m-2 ">
   <label for="exampleFormControlInput1" className="form-label">Email:</label>
-  <input type="email" className="form-control" id="Email" placeholder="email@.com"></input>
+  <input type="email" className="form-control " id="Email" placeholder="email@.com"></input>
   </div>
   <div className="m-2">
   <label for="exampleFormControlTextarea1" className="form-label ">Message:</label>
   <textarea className=" form-control " id="Message" rows="5" placeholder="Message"></textarea>
   </div>
   <div className="m-2">
-  <button className="btn btn-primary w-100 p-2 mt-3" type="button">Send</button>
+  <button className="btn contactBtn btn-secondary w-25 p-2 mt-3" type="button">Send</button>
+ </div>
  </div>
   </>
 ); } 
   
+
+
 
 /* NAVAGATION  */
 function Mynav(){
   return(
 <>
    <ul className=" container nav nav-container pl-2 ">
-      <a className="navbar-brand " href="/Home">MyTasks</a>
+      <li className="navbar-brand ">MyTasks</li>
+      <li className="nav-item">
+         <a className="nav-link" href="/Home">Welcome</a>
+      </li>
       <li className="nav-item">
          <a className="nav-link" href="/Todo">Todo</a>
       </li>
@@ -158,12 +184,9 @@ function Mynav(){
 
 
 function App() {
-  
   return (
-
    <Router>
     <div className="container d-flex flex-column w-100 ">
-    
     <Mynav/>
    
     <Switch>
@@ -171,7 +194,6 @@ function App() {
     <Route path="/Todo" component={Todo} />
     <Route path="/Contact" component={Contact}/>
     </Switch>
-
   </div> 
   </Router>
   ); }
@@ -181,6 +203,5 @@ ReactDOM.render(
   <App/> ,
   document.getElementById('root'),
   );
-
 
   export default App;
