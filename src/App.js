3de6f347dd486c53f18c782570898dc1d2bfda95
Mubeena <77896011+mubeena2021@users.noreply.github.com>
2021-07-29 14:21:44 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function Home(){
   </>
   ); }
 
-  /* TODO PAGE */
+/* TODO PAGE */
 function Todo(){
 
 const [query, setQuery] = useState("");
@@ -26,8 +26,13 @@ const [itemList, updateList] = useState([]);
 const [filter, setFilter] = useState([]);
 
 
+
+
 //Adds task when add button is clicked
-const addToList = () => {
+const addToList = () => { 
+  if(query == ""){
+    return alert("Please enter a task!");
+  }
 updateList([...itemList,{ item:query, completed: false, key:Date.now()} ]);
 setQuery("");
 };
@@ -52,8 +57,8 @@ return(
   
     <div className="container d-flex justify-content-between input-wrapper">
 
-    <input value={query} onChange={updateQuery} onKeyPress={keyPressed} className="form-control w-75" type="text"></input>
-    <button onClick={addToList} className="btn btn-secondary w-25">Add</button>
+    <input value={query} onChange={updateQuery} onKeyPress={keyPressed} className="form-control w-100" type="text"></input>
+    <button onClick={addToList} className="btn btn-secondary rounded ">Add</button>
     </div>
   </div>
     <View itemslist = {itemList} updateList={updateList}  filter={filter} setFilter={setFilter} />
@@ -64,47 +69,70 @@ return(
 ); }
 
 
-const allTasks = (e) => {
-    e.itemslist.map((k) =>{
-console.log(k, "ALL button is clicked")
-})}
-   
 
- const completeTasks = (e) => {
-  e.itemslist.map((k) =>{
-   console.log(k, "complete button is clicked");
-  })}
 
-  const incompleteTasks = (e) => {
-    e.itemslist.map((k) =>{
-     console.log(k, "incomplete button is clicked");
-    })}
 
 
 function View(props){
+  const [all, allitems] = useState("");
+  const [allcomplete, allcompleteditems] = useState("");
+  const [alluncomplete, alluncompleteditems] = useState("");
+  
+
+const allTasks = () => {
+ let c = props.itemslist.map((k) =>{
+return k.item, console.log(k.item , "ALL ")
+})
+allitems(c); console.log(c)
+};
+
+
+const completeTasks = (e) => {
+  e.itemslist.filter((k) => {
+if(k.completed !== false) {
+return console.log(k)
+}
+})}
+
+
+ const incompleteTasks = (e) => {
+    e.itemslist.map((k) =>{
+     if(k.completed !== true) {
+       return console.log(k)
+}
+})}
+
+
+
+
+
  console.log(props)
  // View of all, incomplete and completed tasks
-  return(
+
+   return(
     <>
-      <div className="container ">
-        <div className=" container filter-btn ">
-         <button onClick={ ()=>allTasks(props)}  className="btn btn-secondary " >
+  <div className="container ">
+       <div className=" container filter-btn ">
+         <button onClick={ ()=>allTasks(props)}  className="btn-lg btn-secondary border border-dark rounded ">
           All 
           </button>
-        <button onClick={ ()=>completeTasks(props)} className="btn btn-secondary " >
+        <button onClick={ ()=>completeTasks(props)}  className="btn-lg btn-secondary border border-dark rounded ">
           Completed 
         </button>
-        <button onClick={ ()=>incompleteTasks(props)}className="btn btn-secondary " >
+        <button onClick={ ()=>incompleteTasks(props)} className="btn-lg btn-secondary border border-dark rounded ">
           Incomplete
         </button>
         </div>
-    </div>  
+    </div> 
+   
+
 </>
 );}
 
 
 // Creates list of tasks
 function List(props){
+
   const deleteItem = (key) => {
   const newlist = props.itemslist.filter((item) => {
    return item.key !== key;
@@ -129,7 +157,7 @@ return (
 <>
 {props.itemslist.map((key) =>{
   return <div className="container ">
-         <div className="container taskInputBox w-75  ">
+         <div className="container taskInputBox w-75 ">
             <p className={`taskBox key={key.key} ${key.completed ? "completed ": ''}`}> {key.item} </p>
             <button onClick={ ()=>completeItem(key.key) }className=" complete-Btn btn btn-sm btn-success ">Complete</button>
             <button onClick={ ()=>deleteItem(key.key)} className=" del-Btn btn btn-sm btn-light">Delete</button>      
@@ -149,22 +177,22 @@ return (
   <>
   <div className="container contactContainer ">
   <div className ="m-2 ">
-  <label for ="exampleFormControlInput1" className ="form-label"> First Name:</label>
-  <input type ="text" className ="form-control  " id ="FirstName" placeholde r="First Name"></input>
+  <label for ="exampleFormControlInput1" className ="form-label text-light"> First Name:</label>
+  <input type ="text" className ="form-control  " id ="FirstName" placeholder="First Name"></input>
   </div>
   <div className="m-2 ">
-  <label for="exampleFormControlInput1" className="form-label">Last Name:</label>
-  <input type="text" className="form-control " id="LastName" placeholder="Last Name:"></input>
+  <label for="exampleFormControlInput1" className="form-label text-light">Last Name:</label>
+  <input type="text" className="form-control " id="LastName" placeholder="Last Name"></input>
   </div>
   <div className="m-2 ">
-  <label for="exampleFormControlInput1" className="form-label">Email:</label>
-  <input type="email" className="form-control " id="Email" placeholder="email@.com"></input>
+  <label for="exampleFormControlInput1" className="form-label text-light">Email:</label>
+  <input type="email" className="form-control " id="Email" placeholder="Email"></input>
   </div>
   <div className="m-2">
-  <label for="exampleFormControlTextarea1" className="form-label ">Message:</label>
+  <label for="exampleFormControlTextarea1" className="form-label text-light">Message:</label>
   <textarea className=" form-control " id="Message" rows="5" placeholder="Message"></textarea>
   </div>
-  <div className="m-2">
+  <div className="m-2 ">
   <button className="btn contactBtn btn-secondary w-25 p-2 mt-3" type="button">Send</button>
  </div>
  </div>
